290cb681ae407272bc99b5e4c29c7b551a2ae1c5
Drew Hainer <57354235+CodeConfidant@users.noreply.github.com>
2021-09-19 19:32:27 -0400

    Package update.

--- setup.py
+++ setup.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python3
 
-# Tar Wrap the Package: python setup.py sdist
-# Upload to PYPI: twine upload dist/*
-
 from setuptools import setup, find_packages
+from pathlib import Path
 
 setup(
     name='connectwrap',
-    version='1.1.5',
+    version='1.1.6',
     packages=find_packages(),
     license='MIT',
-    description='A Python package for SQLite database management & object relational mapping.',
-    long_description=open('README.txt').read(),
+    description='Python package made to manage, display & parse data from SQLite file databases.',
+    long_description=(Path(__file__).parent / "README.md").read_text(),
     long_description_content_type='text/markdown',
     url='https://github.com/CodeConfidant/connectwrap-sqlite3',
     author='Drew Hainer',
     author_email='codeconfidant@gmail.com',
-    platforms=['Windows', 'Linux']
+    platforms=['Windows', 'Linux'],
+    python_requires='>=3.6'
 )
+
+# - Update README.md
+# - Update Version Number
+# - Tar Wrap the Package: python setup.py sdist
+# - Check Package: twine check dist/*
+# - Upload to PYPI: twine upload dist/*
+# - Commit Changes
+# - Change Release Version in Github Repo
